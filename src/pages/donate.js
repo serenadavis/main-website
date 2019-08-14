a463e1dc89a7474b8ca68de16c3e4d866709c5ba
Daniel Inge <daninge98@gmail.com>
2019-08-14 02:14:07 -0400

    Updated donate page text

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -5,19 +5,13 @@ export default () => (
     <Layout>
         <div className="container">
             <h1 className="header">DONATE</h1>
-            <p className="text">
-                Thank you for considering a donation to The Harvard Advocate. All donors at Muse and above will, pending permission, have their names featured in an original artwork done by an Advocate alum to be displayed in 21 South Street. All Midas donors will additionally receive a lifetime subscription to The Advocate.
+            <p className="text text-center">
+                Thank you for considering a donation to The Harvard Advocate!
             </p>
-            <p className="centertext">Herodotus $50+</p>
-            <p className="centertext">Odysseus $100+</p>
-            <p className="centertext">Phaedrus $500+</p>
-            <p className="centertext">Penelope $1000+</p>
-            <p className="centertext">Muse $5,000+</p>
-            <p className="centertext">Midas $10,000+</p>
             <p></p>
-            <p className="text">
-                All gifts to The Harvard Advocate can be made through The Trustees of The Harvard Advocate, and are fully tax deductible according to 501(c)(3) non-profit donation guidelines. Checks should be made out to "The Trustees of The Harvard Advocate" and sent to 21 South St., Cambridge, MA 02138.
-                We also offer subscriptions, which we encourage as an alternative to small donations. To arrange a subscription, visit the <u><a href="/subscribe">Subscribe</a></u> page or email us at <a href = "mailto:president@theharvardadvocate.com">president@theharvardadvocate.com</a>.
+            <p className="text text-center">
+                All gifts to The Harvard Advocate are fully tax deductible according to 501(c)(3) non-profit donation guidelines.
+                Donations can be made online using the form below. We also accept donations by check, which should be made out to "The Trustees of The Harvard Advocate" and sent to 21 South St., Cambridge, MA 02138.
                 Thank you for your support!
             </p>
             
