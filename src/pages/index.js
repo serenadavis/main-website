3b02d0a6b00af9762d93967b8c6d93192ca5023f
Daniel Inge <daninge98@gmail.com>
2019-08-16 21:46:07 +0100

    Fixed homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import SmallArticleDisplay from "../components/small_article_display"
+import { Link } from "gatsby"
+
 export const query = graphql`
 {
     art: allMarkdownRemark(limit: 5, filter: {frontmatter: {section: {eq: "art"}}}, sort: {fields: frontmatter___date, order: DESC}) {
@@ -68,6 +70,21 @@ export const query = graphql`
         siteMetadata {
             mediaUrl
         }
+    },
+    editors_picks: allMarkdownRemark(limit: 10) {
+        edges {
+            node {
+            frontmatter {
+                title
+                authors
+                slug
+                section
+                issue
+                date
+            }
+            excerpt
+            }
+        }
     }
 }
 `
@@ -76,7 +93,7 @@ export default ({data}) => (
     <Layout>
         <div class="homepage">
             <section class="top-features">
-                <a href='features'>
+                <Link to={"/content/"+data.features.edges[0].node.frontmatter.slug}>
                     <div class="container-left-half" id="feature-1">
                         <div class="category-rule">
                         <h4 class="category-label">Featured</h4>
@@ -106,102 +123,10 @@ export default ({data}) => (
                     <SmallArticleDisplay data={data.poetry.edges[0]} metadata={data.metadata}/>
                     <SmallArticleDisplay data={data.art.edges[4]} metadata={data.metadata}/>
                 </div>
-                </a>
+                </Link>
             </section>
-        </div>
-        </Layout>)
-            {/* <div class="container-right-half">
-            <a href='{% url "article" feature_2.id feature_2.slug %}'>
-                <div class="container-left-quarter" id="feature-2">
-                <div class="category-rule">
-                    <h4 class="category-label">{{ feature_2.section }}</h4>
-                </div>
-                <div class="feature-title-small">
-                    {{feature_2.title}}
-                </div>
-                <div class="feature-description-small">
-                    {{feature_2.teaser| safe | striptags | truncatewords:30}}
-                </div>
-                <div class="feature-author">
-                    {% for author in feature_2.contributors.all %}
-                        <a href="{{ author.get_absolute_url }}">{{ author.name }}</a>
-                    {% empty %}
-                        <h3 class="subheader-article-author"> No Author </h3> 
-                    {% endfor %}
-                </div>
-                <div class="feature-release-date-small">{{ feature_2.publishDate |date:"M d Y" }}</div>
-                </div>
-            </a>
-            <a href='{% url "article" feature_3.id feature_3.slug %}'>
-                <div class="container-right-quarter" id="feature-3">
-                <div class="category-rule">
-                    <h4 class="category-label">{{ feature_3.section }}</h4>
-                </div>
-                <div class="feature-title-small">
-                    {{ feature_3.title }}
-                </div>
-                <div class="feature-description-small">
-                    {{feature_3.teaser| safe | striptags | truncatewords:27}}
-                </div>
-                <div class="feature-author">
-                    {% for author in feature_3.contributors.all %}
-                        <a href="{{ author.get_absolute_url }}">{{ author.name }}</a>
-                    {% empty %}
-                        <h3 class="subheader-article-author"> No Author </h3> 
-                    {% endfor %}            
-                </div>
-                <div class="feature-release-date-small">{{ feature_2.publishDate |date:"M d Y"  }}</div>
-                </div>
-            </a>
-            <div class="clearfix"></div>
-
-            <a href='{{ feature_4.get_absolute_url }}'> 
-            <div class="container-left-quarter" id="feature-4">
-                <div class="category-rule">
-                    <h4 class="category-label">{{ feature_4.section }}</h4>
-                    </div>
-                    <div class="feature-title-small">
-                        {{feature_4.title}}
-                    </div>
-                    <div class="feature-description-small">
-                        {{feature_4.teaser|safe|striptags|truncatewords:30}}
-                    </div>
-                    <div class="feature-author">
-                        {% for author in feature_4.contributors.all %}
-                        <a href="{{ author.get_absolute_url }}">{{ author.name }}</a>
-                        {% empty %}
-                        <h3 class="subheader-article-author"> No Author </h3> 
-                        {% endfor %}
-                    </div>
-                    <div class="feature-release-date-small">{{ feature_4.publishDate |date:"M d Y" }}</div>
-            </div>
-            </a>
-            <a href="{{ feature_5.get_absolute_url }}">
-            <div class="container-right-quarter" id="feature-5">
-                <div class="category-rule">
-                <h4 class="category-label">Blog</h4>
-                </div>
-                <div class="feature-title-small">
-                {{feature_5.title}}
-                </div>
-                <div class="feature-description-small">
-                {{ feature_5.body | striptags | truncatewords:13}}
-                <div class="feature-author">
-                    {% for author in feature_5.authors.all %}
-                    <a href="{{ author.get_absolute_url }}">{{ author.name }}<br></a>
-                    {% empty %}
-                    <h3 class="subheader-article-author"> No Author </h3> 
-                    {% endfor %}
-                </div>
-                <div class="feature-release-date-small">{{ feature_5.created|date:"M d Y" }}</div>
-                </div>
-            </div>
-            </a> */}
-        {/* </section>
-
         <div class="clearfix"></div>
-
-        <section class="banner-advertisement">
+        {/* <section class="banner-advertisement">
             {% if ads %} {% for ad in ads %}
             <div class="col-md-4">
                 <article>
@@ -212,7 +137,60 @@ export default ({data}) => (
                     </figure>
             </div>
             {% endfor %} {% endif %}
+        </section>         */}
+        <section class="bottom-features">
+            <div class="container-left-half">
+                <div id="feature-current-issue">
+                    <div class="category-rule">
+                    <h4 class="category-label">Current Issue</h4>
+                    </div>
+                    <div class="current-issue-label">
+                    <a href='{% url "issue" issue.issue issue.year %}'>Spring 2019</a>
+                    </div>
+                    <a href='{% url "issue" issue.issue issue.year %}'>
+                    <div class="feature-image-container-current-issue">
+                        <div class="feature-image" id="feature-current-issue-image">
+                            <img style={{width: "100%"}} src={data.metadata.siteMetadata.mediaUrl + "issue_covers/2019/spring_2019_cover.png"}/>
+                        </div>
+                    </div>
+                    </a>
+                    <div class="current-issue-shop">
+                    <Link to="/shop">See Shop</Link>
+                    </div>
+                </div>
+
+                <SmallArticleDisplay data={data.features.edges[2]} metadata={data.metadata}/>
+                <SmallArticleDisplay data={data.fiction.edges[3]} metadata={data.metadata}/>
+                <div class="clearfix"></div>
+            </div>
+            <div class="container-right-half">
+                <div id="editors-picks">
+                    <div class="category-rule">
+                    <h4 class="category-label">From the Archives</h4>
+                    </div>
+                    <ol class="homepage-list">
+                    { data.editors_picks.edges.map(pick => (
+                        <li class="feature-list-item">
+                        <div class="feature-list-item-title">
+                            <Link to={"/content/"+pick.node.frontmatter.slug}>{pick.node.frontmatter.title}</Link>
+                        </div>
+                        <div class="feature-list-item-author">
+                            {pick.node.frontmatter.authors.map(author => (
+                                <a href="{% url 'contributor' author.id author.name %}"><span>{author}</span></a>
+                            ))}
+                        </div>
+                        <div class="feature-list-item-release-date">{pick.node.frontmatter.date}</div>
+                        </li>
+                    ))}
+                    </ol>
+                </div>
+            </div>
         </section>
+        </div>
+        </Layout>)
+        {/* </section>
+
+        <div class="clearfix"></div>
 
         <section class="bottom-features">
             <div class="container-left-half">
