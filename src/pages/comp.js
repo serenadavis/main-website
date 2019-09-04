3ebed42f6b466a458f61881b8c92a80da1a1d628
Daniel Inge <daninge98@gmail.com>
2019-09-04 21:15:43 +0100

    added fall 2019 comp info

--- src/pages/comp.js
+++ src/pages/comp.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
-
+import Image from "../components/image"
 export default () => (
    <Layout>
         <div class="container">
-            <h1 class="header">FALL 2019 COMP INFORMATION COMING SOON</h1>
-                {/* <figure>
-                    <img class="img-responsive" src="{{ MEDIA_URL }}sites/default/files/spring_2019_comp_banner.jpg" alt="Main Image" style="margin:auto; width:75%" align="middle"/>
-                </figure> */}
+            <h1 class="header">FALL 2019 COMP</h1>
+                <figure>
+                    <Image name="fall-2019-comp.jpg" style="margin:auto; width:75%" align="middle"/>
+                </figure>
 
             <p class="text">
                 Between its seven boards:  Art, Business, Design, Features, Fiction, Poetry and Technology, The Harvard Advocate 
@@ -21,8 +21,22 @@ export default () => (
                 comp is selective, our hope is that all participants will find it an enriching and rewarding experience. Please direct specific 
                 questions about the comp to the <Link to="about">editor of the board</Link> that you are interested 
                 in comping.
-
-                If you are interested in learning more about our comp, please contact the relevant board head:
+            </p>
+            <p class="text">
+            For more information about the Advocate and this semester’s comp, come to one of our introductory meetings, held at our building at 21 South Street:
+            </p>
+            <div className="center">
+            <p class="text">
+            Tuesday 9/10 at 9pm 
+            </p>
+            <p class="text">
+            Wednesday 9/11 at 9pm
+            </p>
+            <p class="text">
+            Thursday 9/12 at 9pm
+            </p>
+            </div>
+                {/* If you are interested in learning more about our comp, please contact the relevant board head:
                 <div class="center">
                     <ul>
                         <li style={{listStyleType: "none"}}>art@theharvardadvocate.com</li>
@@ -33,9 +47,7 @@ export default () => (
                         <li style={{listStyleType: "none"}}>poetry@theharvardadvocate.com</li>
                         <li style={{listStyleType: "none"}}>tech@theharvardadvocate.com</li>
                     </ul>
-                </div>
-            </p>
-
+                </div> */}
         </div>
    </Layout>
 )
