9d63c36a40100b50eb6df2e799b9d5e54a858d98
Daniel Inge <daninge98@gmail.com>
2019-11-04 15:24:02 -0500

    Added feast description to submit page

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -6,9 +6,16 @@ export default () => (
         <div class="container">
             <h1 class="header">SUBMISSION GUIDELINES</h1>
             <div className="center">
-                <p><i>The Harvard Advocate</i> accepts submissions of art, fiction, and poetry of any form or length created by members of the Harvard community, as well as outside contributors at our discretion. All submissions are considered anonymously. Submissions may be uploaded online:</p>
-                <p>We are currently accepting submissions for our Winter 2019 issue "Feast". Our winter review period ends on December 6, 2019.</p>
+                <p><i>The Harvard Advocate</i> accepts submissions of art, fiction, and poetry of any form or length created by members of the Harvard community, as well as outside contributors at our discretion. All submissions are considered anonymously</p>
             </div>
+            <h1 class="header">Winter 2019 Issue</h1>
+            <div className='center'>
+                <p>We are currently accepting submissions for our Winter 2019 issue "Feast":</p>
+                <p><i>What do we choose to feast on? What are we eating to fill? In this themed issue, we want to capture the quiet, starved anticipation before the meal, the gluttony of collective consumption, the radiance and laziness of being full. We want to explore feast as a warm, familial ritual, hands turning a lazy susan round and round. What happens when individual hunger turns communal? What does the desire to be fed allow us to rationalize? Feast is the violence of squeaky silverware, the squelch of chicken legs parting with undercooked flesh. Feast is warm soup. Feast is the fancy china that is not meant for eating, that you are not allowed to touch, that your mother assures you is meant for something more.</i></p>
+                <p>Our Winter 2019 review period ends on December 6, 2019.</p>
+            </div>
+            <p></p>
+            <p></p>
             <div class="center"> 
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdHCYzDkX4ZAuip4NwjCeWXODfQSrheQDGEcgFApdhLrF35sg/viewform?usp=sf_link" class="btn btn-primary btn-150th">FICTION</a>
             </div>
